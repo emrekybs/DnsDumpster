22561cd1789bb3f021c8b2d76ffe8cd455e869e8
emrekybs <50271518+emrekybs@users.noreply.github.com>
2019-05-04 18:25:04 +0200

    project pertama

---
+++ port_tarama.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet PORT TARAMA")
+print(""" 
+(EMRE KYBS) PORT TARAMA ARACINA HOŞGELDİNİZ :)
+
+1)Hızlı Tarama
+2)Servis ve Versiyon Bilgisi
+3)Isletim Sistemi Bilgisi
+
+
+
+""")
+
+islemno = input("Islem Numarasini Girin: ")
+
+if(islemno=="1"):
+          hedefip = input("Hedef Ip Girin: ")
+          os.system("nmap " + hedefip)
+elif(islemno=="2"):
+          hedefip = input("Hedef Ip Girin: ")
+          os.system("nmap -sS sV " + hedefip)
+elif(islemno=="3"):
+          hedefip = input("Hedef Ip Girin: ")
+          os.system("nmap -0 " + hedefip)
+else: 
+       print("Hatalı Seçim Yaptın Dostum :(")
+
