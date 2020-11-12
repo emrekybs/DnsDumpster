0c2f12af9b42a289dbf77904d277f1dbd66ef481
Emre Koybasi <emrekoybasi@protonmail.com>
2020-11-12 13:12:02 +0300

    Add files via upload

---
+++ dnsdumpster.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+#EmreKybs - DNSDUMPESTER
+import sys
+import requests
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import urllib.parse
+import re
+import os
+
+if len(sys.argv) !=3:       
+   print("DnsDumpster") 
+                                        
+   print("\033[1;39m | By Emre Koybasi| \033[1;39m ")
+def getcsrf(t):
+	d=t[(t.find("csrf")+28):(t.find("csrf")+60)]
+	return d
+total=[]
+def eliminate(l):
+        if(l not in total):
+                total.append(l)
+                
+url="https://dnsdumpster.com/"
+print('''\033[1;39m''')
+dom=input("Domain: ")
+print ('''  ''')
+r=requests.get(url)
+t=r.text
+tkn="csrftoken="+getcsrf(t)+";"
+header = {"Referer":"https://dnsdumpster.com","Cookie" :tkn}
+r=requests.post("https://dnsdumpster.com",data={'csrfmiddlewaretoken':getcsrf(t),'targetip':dom},headers=header)
+resp=r.text
+l=re.findall((r'<tr><td class="col-md-4">(.*?).'+dom+'<br>'),resp)
+for i in l:
+        if(i!=''):
+                v=i+'.'+dom
+                eliminate(v)
+                                             
+link=[]
+url="https://www.virustotal.com/en/domain/"+dom+"/information/"
+r=requests.get(url)
+t=r.text
+f=open(".Dns.txt","w")
+f.write(t)
+f.write("Dns")
+f.close()
+f=open(".Dns.txt","r")
+a="/"
+n=0
+for i in f.readlines():
+           if i=="  <div class=\"enum \">\n":
+                                            f.readline()
+                                            a=f.readline()
+                                            a.lstrip()
+                                            if(a[-3:-1]=="om"):
+                                                               
+                                                               link=link+[a[100:-100]]
+                                                               n+=1
+for i in link:
+    eliminate(i)
+for i in total:
+        print(i)
+        
+
+
